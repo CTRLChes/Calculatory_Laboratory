347b04b8d421ce667c0b455e6a1bb287bf1db1d6
CTRLChes <leoisntgood@gmail.com>
2026-04-22 18:23:14 +0800

    Update activity.js

--- activity.js
+++ activity.js
@@ -1,8 +1,30 @@
-//step 1 Start by creating a function called addTwoAndSeven.
-//step 2 Inside your addTwoAndSeven function, return the sum of 2 and 7.
-const addTwoAndSeven = (num1, num2) => {
-  return num1 + num2;
+// Start by creating a function called addTwoAndSeven.
+// Inside your addTwoAndSeven function, return the sum of 2 and 7.
+const addTwoAndSeven = () => {
+  return 2 + 7;
 }
 
-console.log(addTwoAndSeven(2,7));
+// Declare a function called addThreeAndFour that returns the sum of 3 and 4.
+// Then call the addThreeAndFour function inside a console.log to see the result.
+const addThreeAndFour = () => {
+  return 3 + 4;
+}
 
+console.log(addThreeAndFour());
+
+
+// Declare a function called calculateSum that takes two parameters, num1 and num2.
+function calculateSum(num1, num2) {
+  return num1 + num2;
+}
+// Add a console.log that calls the calculateSum function with the arguments 2 and 5.
+console.log(calculateSum(2, 5));  
+//Add another console.log that calls the calculateSum function with the arguments 10 and 10.
+console.log(calculateSum(10, 10)); 
+//Below that console.log, add another console.log that calls the calculateSum function with the arguments 5 and 5.
+console.log(calculateSum(5, 5));   
+
+// Declare a function called calculateDifference that takes two parameters, num1 and num2.
+function calculateDifference(num1, num2) {
+  return num1 - num2;
+}
