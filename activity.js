5f3a167bce3e4783c6f45e41630ac67550958c15
CTRLChes <leoisntgood@gmail.com>
2026-04-22 18:42:35 +0800

    Update activity.js

--- activity.js
+++ activity.js
@@ -28,10 +28,48 @@ console.log(calculateSum(5, 5));
 function calculateDifference(num1, num2) {
   return num1 - num2;
 }
-
 // Start by adding a console.log that calls the calculateDifference function with the arguments 22 and 5.
 console.log(calculateDifference(22, 5))
 // Add a second console.log that calls the calculateDifference function with the arguments 12 and 1.
 console.log(calculateDifference(12, 1))
-// add a third console.log that calls the calculateDifference function with the arguments 17 and 9.
+// add a third console.log that calls the calculateDifference function with the arguments 17 and 9.  
 console.log(calculateDifference(17, 9))
+
+// Declare a function called calculateProduct that takes two parameters, num1 and num2.
+function calculateProduct = (num1,num2) => {
+  return num1 * num2;
+}
+// Add a console.log that calls the calculateProduct function with the arguments 13 and 5.
+console.log(calculateProduct(13, 5));
+
+// Declare a function called calculateQuotient that takes two parameters, num1 and num2.
+function calculateQuotient(num1, num2) {
+  if (num2 === 0) {
+    return "Error: Division by zero";
+  }
+  return num1 / num2;
+}
+// Add a console.log that calls the calculateQuotient function with the arguments 7 and 11.
+console.log(calculateQuotient(7,11));
+// Add a console.log that calls the calculateQuotient function with the arguments 3 and 0.
+console.log(calculateQuotient(3,0));
+
+// Declare a function called calculateSquare that takes a num parameter and returns the square of num.
+function calculateSquare(num) {
+  return num ** 2;
+// Add a console.log that calls the calculateSquare function with the argument of 2.
+console.log(calculateSquare(2));
+// Add another console.log that calls the calculateSquare function with the argument of 9.
+console.log(calculateSquare(9));
+
+// Declare a function called calculateSquareRoot that takes a num parameter and returns the square root of num.  
+function calculateSquareRoot(num) {
+  return Math.sqrt(num);
+}
+// Add a first console.log that calls the calculateSquareRoot function with the argument of 25.
+console.log(calculateSquareRoot(25)); 
+// Add a second console.log that calls the calculateSquareRoot function with the argument of 100.  
+console.log(calculateSquareRoot(100));
+
+
+
