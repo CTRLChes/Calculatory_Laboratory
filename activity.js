620416f46a06ec52ec832c12a0d6af0bf16f23f9
CTRLChes <leoisntgood@gmail.com>
2026-04-22 18:09:59 +0800

    Update activity.js

--- activity.js
+++ activity.js
@@ -1,4 +1,5 @@
-
+//step 1 Start by creating a function called addTwoAndSeven.
+//step 2 Inside your addTwoAndSeven function, return the sum of 2 and 7.
 const addTwoAndSeven = (num1, num2) => {
   return num1 + num2;
 }
